ef414a25c108154103157e1bac65aa08a7ca8df1
Bhumika Dhote <dhotebhumika3@gmail.com>
2026-02-08 15:26:18 +0530

    Delete reports.py

--- reports.py
+++
@@ -1,8 +0,0 @@
-def total_expenses(expenses):
-    return sum(exp.amount for exp in expenses)
-
-def category_summary(expenses):
-    summary = {}
-    for exp in expenses:
-        summary[exp.category] = summary.get(exp.category, 0) + exp.amount
-    return summary
