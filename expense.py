637b13f3018e6af3c10cd5377c79936a2e5cdda2
Bhumika Dhote <dhotebhumika3@gmail.com>
2026-02-08 15:23:52 +0530

    Delete expense.py

--- expense.py
+++
@@ -1,9 +0,0 @@
-class Expense:
-    def __init__(self, amount, category, date, description):
-        self.amount = float(amount)
-        self.category = category
-        self.date = date
-        self.description = description
-
-    def __str__(self):
-        return f"{self.date} | {self.category}: ₹{self.amount} - {self.description}"
